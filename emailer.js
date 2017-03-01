85a7a69e3c2af84d4534409681aacd193d5e7f47
System <none>
2017-03-01 13:58:50 +0000

    🐊💉 Updated with Gomix

--- emailer.js
+++ emailer.js
@@ -10,15 +10,14 @@ const {SMTP_URL, ALERT_FROM_EMAIL, ALERT_FROM_NAME, ALERT_TO_EMAIL} = process.en
 const emailData = {
   from: `${ALERT_FROM_NAME}<${ALERT_FROM_EMAIL}>`,
   to: ALERT_TO_EMAIL,
-  subject: "There was an error",
-  text: "An error occured (plain text)",
-  html: "<p>An error occured (html)</p>"
+  subject: `There was an error`,
+  text: `An error occured (plain text)`,
+  html: `<p>An error occured (html)</p>`
 };
 
 const sendEmail = (emailData, smtpUrl=SMTP_URL) => {
   const transporter = nodemailer.createTransport(SMTP_URL);
   logger.info(`Attempting to send email from ${emailData.from}`); 
-  console.log(`The FROM is: ${emailData.from}`);
   return transporter
     .sendMail(emailData)
     .then(info => console.log(`Message sent: ${info.response}`))
--- server.js
+++ server.js
@@ -33,9 +33,7 @@ app.get('*', russianRoulette);
 //app.use(sendEmail);
 
 app.use((err, req, res, next) => {
-  logger.error(`The error is ${err.name}`);
   if (err.name == 'FooError' || err.name == 'BarError') {
-    console.log("send mail");
     emailData.subject = `ALERT: a ${err.name} error occurred`;
     emailData.html = `<h3>An error occured</h3><p>A ${err.name} error occurred and returned the following message: <blockquote>${err.message}.</blockquote></p><p>The error stack is: <blockquote>${err.stack}.</blockquote><p/>`;
     emailData.text = `An error occured\n\nA ${err.name} error occurred and returned the following message: ${err.message}.\n\nThe error stack is: ${err.stack}.`;
